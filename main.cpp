7ccdb5830c376fda906472826347b1f34c360a12
Gaofei244617 <gaofeibuaa@outlook.com>
2020-08-24 00:32:37 +0800

    更正测试demo

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main()
 
     for (int i = 0; i < 100; i++)
     {
-        LOGINFO(log) << "In log-1 " << i << ", i * i = " << i * i;
-        LOGINFO(log2) << "In log-2 " << i << ", i + i = " << i + i;
+        TIDLOG_INFO(log) << "In log-1 " << i << ", i * i = " << i * i;
+        TIDLOG_DEBUG(log2) << "In log-2 " << i << ", i + i = " << i + i;
     }
+
     return 0;
 }
